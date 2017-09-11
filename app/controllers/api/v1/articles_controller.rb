b4718df1f69ae55b1b8a956c144f260c359d1c12
Sylvain-Kraisin <sylvain.kraisin@gmail.com>
2017-09-11 14:33:36 +0200

    first commit

---
+++ app/controllers/api/v1/articles_controller.rb
@@ -0,0 +1,19 @@
+module Api
+  module V1
+    class ArticlesController < ApplicationController
+      def index
+      end
+
+      def show
+      end
+
+      def destroy
+      end
+
+      private
+        def find_article
+        end
+
+    end
+  end
+end
